48c41e94c0c7f8012c19e704ed93475a67caa686
laliadeeb <sftrain22@gmail.com>
2022-10-27 16:04:29 -0700

    Update streamlit_app.py
    add more item

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.text('🍞Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinack & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑 HAvocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
