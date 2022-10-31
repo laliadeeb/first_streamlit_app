f7028362221be5202be3e931ad89612df4264c79
laliadeeb <sftrain22@gmail.com>
2022-10-31 13:10:09 -0700

    Update streamlit_app.py
    query from snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,3 +47,11 @@ streamlit.dataframe(fruityvice_normalized)
 
 # Lesson 12 adding Snowflake connector using Pandas python
 import snowflake.connector
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cns.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hellow from Snowflake:")
+streamlit.text(my_data_row)
+
