1b75c744331c4d3487c84b68a48d4478829f6d2c
laliadeeb <sftrain22@gmail.com>
2022-10-31 09:34:54 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,6 +32,6 @@ streamlit.dataframe(fruits_to_show)
 # New Section to display fruityvice API Response
 streamlit.header('Fruityvice Fruit Advice!')
 
-import Requests
+import Request
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
