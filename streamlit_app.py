4a9075b06629408dfb5001c872b13df674914fd9
laliadeeb <sftrain22@gmail.com>
2022-10-31 14:38:41 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,6 +64,6 @@ fruit_choice = streamlit.text_input('What fruit would you like to add?', 'Jackfr
 
 my_cnx2 = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur2 = my_cnx2.cursor()
-my_cur2.execute("INSERT INTO pc_rivery_db.public.fruit_load_list values(fruit_choice)")
+my_cur2.execute("INSERT INTO pc_rivery_db.public.fruit_load_list " "values(?)", (fruit_choice))
 
 streamlit.text('Thank you for adding ' + fruit_choice)
